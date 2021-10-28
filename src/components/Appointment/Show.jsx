e91c91e8a6a2d2a0a550e0481139f1a0ffecb330
Nikolaj Johansen <nikolaj.juuel@gmail.com>
2021-10-27 21:40:42 -0700

    ads show

---
+++ src/components/Appointment/Show.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Appointment from "components/Appointment";
+import "components/Appointment/styles.scss";
+
+export default function show(props) {
+
+    return (
+        <main className="appointment__card appointment__card--show">
+            <section className="appointment__card-left">
+                <h2 className="text--regular">Lydia Miller-Jones</h2>
+                <section className="interviewer">
+                    <h4 className="text--light">Interviewer</h4>
+                    <h3 className="text--regular">Sylvia Palmer</h3>
+                </section>
+            </section>
+            <section className="appointment__card-right">
+                <section className="appointment__actions">
+                    <img
+                        className="appointment__actions-button"
+                        src="images/edit.png"
+                        alt="Edit"
+                        onClick={props.onEdit}
+                    />
+                    <img
+                        className="appointment__actions-button"
+                        src="images/trash.png"
+                        alt="Delete"
+                        onClick={props.onDelete}
+                    />
+                </section>
+            </section>
+        </main>
+    );
+}
+
--- stories/index.js
+++ stories/index.js
@@ -13,7 +13,8 @@ import InterviewerList from "components/InterviewerList";
 
 import Appointment from "components/Appointment/index.jsx";
 import Header from "components/Appointment/Header";
-import Empty from "components/Appointment/Empty"
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 
 storiesOf("Button", module)
@@ -147,4 +148,5 @@ storiesOf("InterviewerList", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
-  .add("Empty", () => <Empty onAdd={action("onAdd")}/>);
+  .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+  .add("Show", () => <Show onEdit={action("onEdit")} onDelete={action("onDelete")}  />);
